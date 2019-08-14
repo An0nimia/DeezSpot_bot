683625918974c0f9f08b7d0eef41d32a5a6a1009
An0nimia <41164945+An0nimia@users.noreply.github.com>
2019-08-14 20:19:16 +0300

    Delete setting.py

--- setting.py
+++
@@ -1,4 +0,0 @@
-username = "" #Your Deezer email
-password = "" #Your Deezer password
-token = "token" #Your Telegram bot token
-deezer_token = "deezer token" #Your Deezer token log (for get it log in Deezer from your PC press f12 and find arl cookie)
