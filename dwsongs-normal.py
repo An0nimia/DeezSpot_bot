fa2483e510239018c43f86d8db5b8dcaffd317d7
9Mad-Max5 <maximilian.huebner95@gmx.de>
2020-10-01 14:46:38 +0200

    correction

--- dwsongs-normal.py
+++ dwsongs-normal.py
@@ -247,7 +247,7 @@ def sendAudio(
 ):
     sleep(default_time)
     try:
-        if server_mode == false:
+        if server_mode == False:
             if os.path.isfile(audio):
                 bot.sendChatAction(chat_id, "upload_audio")
 
